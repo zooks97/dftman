01f20a58f45fd2d57e72d38f26dbbf037cc55d71
Austin Zadoks <zooks97@gmail.com>
2019-01-25 19:58:03 -0500

    Cleaned up a bit

--- lib/pypwio/__init__.py
+++ lib/pypwio/__init__.py
@@ -1,6 +1,9 @@
+from .MPQuery import MPQuery
 from .PWInput import PWInput
 from .PWOutput import PWOutput
 from .PWXML import PWXML
 from .PWCalculation import PWCalculation
+from .SubmitJob import SubmitJob
+from .dftman_utils import *
 
 __all__ = ["PWInput", "PWOutput", "PWXML", "PWCalculation"]
--- lib/pypwio_AlBain/__init__.py
+++
@@ -1,6 +0,0 @@
-from .PWInput import PWInput
-from .PWOutput import PWOutput
-from .PWXML import PWXML
-from .PWCalculation import PWCalculation
-
-__all__ = ["PWInput", "PWOutput", "PWXML", "PWCalculation"]
